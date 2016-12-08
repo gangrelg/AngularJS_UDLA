01d76fe898206126b8577da8ff9a5803e781e741
Galo Mosquera <gangrelg@hotmail.com>
2016-12-08 00:47:34 -0500

    Update_Pago_Student
    Updates pago for student succesfully

--- partials/insertpago.php
+++ partials/insertpago.php
@@ -10,6 +10,21 @@ $estadopago = $dbhandle -> real_escape_string($data -> estadopago);
 
 $matriculastudent = $dbhandle -> real_escape_string($data -> matriculastudent);
 
-$query = "INSERT INTO pago (estadopago, student_id) VALUES ('".$estadopago."', (SELECT student.id from student where matriculastudent = '".$matriculastudent."'))";
+$result = mysqli_query($dbhandle, "SELECT namestudent, matriculastudent, estadopago FROM student, pago WHERE student.matriculastudent = '".$matriculastudent."' AND student.id = pago.student_id AND pago.estadopago = 'pagado'");
+
+$num_rows = mysqli_num_rows($result);
+
+if($num_rows == 1){
+	echo "El estudiante ya ha realizado el pago del periodo!";
+}else{
+
+$query = "UPDATE pago
+  SET pago.estadopago='pagado'
+WHERE pago.student_id =
+  (
+    SELECT student_id
+    FROM student WHERE student.matriculastudent = '".$matriculastudent."'
+  )";
 
 $dbhandle -> query($query);
+}
